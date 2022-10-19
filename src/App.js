cddc28987e8a6a853cc729eb297b37f302502e8d
Rafael Bicalho <rafaeljbicalho@gmail.com>
2022-10-19 11:08:06 -0300

    added icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-// import logo from './logo.svg';
 import logo from './img/umdocedom.jpg';
 import './App.css';
+import Icons from './components/Icons/Icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem-vindo ao site do @umdocedom
-        </p>
-        <a
-          className="App-link"
-          href="https://instagram.com/umdocedom"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nos visite no Instagram
-        </a>
+        <Icons />
       </header>
     </div>
   );
---
+++ src/components/Icons/Icons.js
@@ -0,0 +1,37 @@
+import './Icons.css';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+function Icons() {
+    return(
+        <div className='rowC'>
+            <a
+                className="App-link"
+                href="https://instagram.com/umdocedom"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <InstagramIcon sx={{ fontSize: 30 }}/>
+            </a>
+            <a
+                className="App-link"
+                href="https://facebook.com/umdocedom"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <FacebookIcon sx={{ fontSize: 30 }}/>
+            </a>
+            <a
+                className="App-link"
+                href="https://wa.me/5519982947878"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <WhatsAppIcon sx={{ fontSize: 30 }}/>
+            </a>
+        </div>
+    )
+}
+
+export default Icons;
